654a19eae4a540a9e19746af0a01aede62cabd64
Serge <s@shayderov.ru>
2019-08-17 23:40:35 +0300

    fix require paths

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-require_relative 'card.rb'
-require_relative 'deck.rb'
-require_relative 'hand.rb'
-require_relative 'player.rb'
-require_relative 'dealer.rb'
-require_relative 'money_account.rb'
-require_relative 'game_helper.rb'
-require_relative 'game.rb'
-require_relative 'terminal_interface.rb'
+require_relative 'blackjack/card.rb'
+require_relative 'blackjack/deck.rb'
+require_relative 'blackjack/hand.rb'
+require_relative 'blackjack/player.rb'
+require_relative 'blackjack/dealer.rb'
+require_relative 'blackjack/money_account.rb'
+require_relative 'blackjack/game_helper.rb'
+require_relative 'blackjack/game.rb'
+require_relative 'blackjack/terminal_interface.rb'
 
 game = Game.new(interface: TerminalInterface.new)
 game.main
